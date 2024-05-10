db5dc3a5877e9f30a0b0d7f6d0838cd7d1ff9104
ksemenova17 <ksemenova2412@gmail.com>
2024-05-10 17:23:36 +0300

    'добавила fact.svg на mainpage.jsx'

--- my_app/src/page/mainpage.jsx
+++ my_app/src/page/mainpage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import fact from "../images/home/fact.svg"
 
 function mainpage(){
     return(
@@ -8,15 +9,24 @@ function mainpage(){
                 <h1 className="main-content-title">
                     void horizons
                 </h1>
-                {/*<div className="main-content-subtitle">инновационная, образовательная платформа, направленная на изучениие созвездий и космических объектов. Присоединяйся, чтобы получить полный доступ ко всем функциям на сайте, таким как расчет созвездий по координатам, а также полный список созвездий в библиотеке.</div>*/}
                 <p className="main-content-subtitle-line1">вселенная - это книга, </p>
                 <p className="main-content-subtitle-line2">наполненная неизведанными </p>
                 <p className="main-content-subtitle-line3">страницами. </p>
+                <img className="fact" src= {fact}/>
                 <Link to="/login">
                     <button>регистрация</button>
                 </Link>
-            </div>
 
+                <h1 className="library">
+                    библиотека
+                </h1>
+            </div>
+            {/*<div className="second_section">*/}
+            {/*    <h1 className="library">*/}
+            {/*        библиотека*/}
+            {/*    </h1>*/}
+
+            {/*</div>*/}
         </div>
     )
 }
