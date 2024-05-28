25767fde18b604231deca5949a46e819b9145b1c
ksemenova17 <ksemenova2412@gmail.com>
2024-05-29 02:06:36 +0300

    'интеграция 3d-модели'

--- my_app/src/page/library.jsx
+++ my_app/src/page/library.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function library(){
     return(
         <div>library</div>
--- my_app/src/page/mainpage.jsx
+++ my_app/src/page/mainpage.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import fact from "../images/home/fact.svg"
+import React, {useState, useEffect, useRef, useFrame, Suspense} from "react";
 import Facts from "../components/Facts/Facts.jsx";
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Earth from '../models/Earth'
+
+function RotatingEarth(props) {
+    const ref = useRef();
+    useFrame(() => {
+        if (ref.current) {
+            ref.current.rotation.y += 0.01; // Вращение модели вокруг оси Y
+        }
+    });
+    return <Earth ref={ref} {...props} />;
+}
 
 function mainpage(){
-    return(
+    return (
         <div className="mainpage">
+            <div className="canvas-container">
+                <Canvas camera={{position: [2, 2, 5], fov: 75, near: 0.5, far: 2000}}>
+                    <Suspense fallback={null}>
+                        <OrbitControls/>
+                        <ambientLight intensity={0}/>
+                        <directionalLight intensity={3} position={[1, 0, 1]}/>
+                        <Earth position={[0.5, 0.35, 0]} rotation={[12.65, -0.6, 0]} scale={[0.5, 0.5, 0.5]}/>
+                        RotatingEarth position={[0.5, 0.35, 0]} rotation={[12.65, -0.6, 0]} scale={[0.5, 0.5, 0.5]} />
+                    </Suspense>
+                </Canvas>
+            </div>
             <div className="main-content">
-                <h1 className="main-content-title">
-                    void horizons
-                </h1>
+                <h1 className="main-content-title">void horizons</h1>
                 <p className="main-content-subtitle-line1">вселенная - это книга, </p>
                 <p className="main-content-subtitle-line2">наполненная неизведанными </p>
                 <p className="main-content-subtitle-line3">страницами. </p>
